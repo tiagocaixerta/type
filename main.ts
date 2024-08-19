6c30292b3aee1b470d04539a3345207e6078d385
Tiago <caixerta63@gmail.com>
2024-08-19 09:22:36 -0300

    first

---
+++ main.ts
@@ -0,0 +1,16 @@
+
+function multiplicar(a: number, b: number): number {
+    return a * b;
+}
+
+
+function saudar(nome: string): string {
+    return "Olá " + nome;
+}
+
+
+const resultadoMultiplicacao = multiplicar(3, 4);
+console.log(resultadoMultiplicacao); 
+
+const saudacao = saudar("Tiago");
+console.log(saudacao); 
